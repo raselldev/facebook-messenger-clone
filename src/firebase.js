43dcbabb20004c55c898e3dfb58b37c7f0017c7d
raselldev <rrrraka@gmail.com>
2021-05-26 10:09:54 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyAEns0vJVlBUQWCVTaBTPc1EF-CGaw4dcc",
+    authDomain: "facebook-messenger-clone-95627.firebaseapp.com",
+    projectId: "facebook-messenger-clone-95627",
+    storageBucket: "facebook-messenger-clone-95627.appspot.com",
+    messagingSenderId: "881754538432",
+    appId: "1:881754538432:web:1da873992c5e53b0c34825"
 })
 
 const db = firebaseApp.firestore();
